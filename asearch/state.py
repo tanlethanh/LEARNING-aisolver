a121ad6948e116a18cc1167bbf07f44f9b30478b
HnimTadd <minhdat15012002@gmail.com>
2022-11-04 15:50:10 +0700

    add asearch module

---
+++ asearch/state.py
@@ -0,0 +1,22 @@
+
+class ASState:
+
+    def __init__(self, parent, parent_action) -> None:
+        self.parent = parent
+        self.parent_action = parent_action
+
+    def is_goal(self) -> bool:
+        pass
+
+    def neighbours(self) -> list:
+        pass
+
+    def __eq__(self, o: object) -> bool:
+        pass
+
+    def __lt__(self, o: object) -> bool:
+        pass
+
+    def get_cost(self) -> int:
+        pass
+
